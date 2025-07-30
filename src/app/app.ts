3437683bc56208d057a6c1dfcdaf5b0ae0a313a3
matheustorresdev97 <matheustorresdev@gmail.com>
2025-07-30 11:42:14 -0300

    filtrando por texto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Container } from './components/container/container';
 import { Header } from './components/header/header';
-import { Separador } from "./components/separador/separador";
-import { Contato } from "./components/contato/contato";
+import { Separador } from './components/separador/separador';
+import { Contato } from './components/contato/contato';
+
+import agenda from './agenda.json';
+import { ContatoInterface } from './interfaces/contato';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +17,11 @@ export class App {
   protected readonly title = signal('indexa');
 
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: ContatoInterface[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string): ContatoInterface[] {
+    return this.contatos.filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }

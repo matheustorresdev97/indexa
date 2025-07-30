118ff052f00dd860f877708ba354f92e28e9da7d
matheustorresdev97 <matheustorresdev@gmail.com>
2025-07-30 09:03:45 -0300

    estruturando a agenda

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Container } from "./components/container/container";
+import { Container } from './components/container/container';
+import { Header } from './components/header/header';
+import { Separador } from "./components/separador/separador";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container],
+  imports: [Container, Header, Separador],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('indexa');
+
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
 }

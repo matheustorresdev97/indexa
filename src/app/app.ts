49d1a49389f14e5b4d1ed46e70addd0c63e2d72c
matheustorresdev97 <matheustorresdev@gmail.com>
2025-07-30 11:26:25 -0300

    lendo contatos do JSON

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Container } from './components/container/container';
 import { Header } from './components/header/header';
 import { Separador } from "./components/separador/separador";
+import { Contato } from "./components/contato/contato";
 
 @Component({
   selector: 'app-root',
-  imports: [Container, Header, Separador],
+  imports: [Container, Header, Separador, Contato],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

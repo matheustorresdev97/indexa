214280488ab3c34c16b2d152c566d6892cdd2a15
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-02 19:45:31 -0300

    trabalhando com validators

--- src/app/pages/formulario-contato/formulario-contato.ts
+++ src/app/pages/formulario-contato/formulario-contato.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { Container } from '../../components/container/container';
 import { Separador } from '../../components/separador/separador';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -14,17 +19,19 @@ export class FormularioContato {
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl('Matheus'),
-      telefone: new FormControl('99 9999-9999'),
-      email: new FormControl('matheus@email.com'),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
-      observacoes: new FormControl('Olá Mundo!'),
+      observacoes: new FormControl(''),
     });
   }
 
   salvarContato() {
-    console.log(this.contatoForm.value);
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
   }
 
   cancelar() {

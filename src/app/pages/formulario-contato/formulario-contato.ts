421aeb0121fbbb0d559082b363c691659dd7d6f3
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-02 19:16:02 -0300

    formulario reativo

--- src/app/pages/formulario-contato/formulario-contato.ts
+++ src/app/pages/formulario-contato/formulario-contato.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { Container } from "../../components/container/container";
-import { Separador } from "../../components/separador/separador";
+import { Container } from '../../components/container/container';
+import { Separador } from '../../components/separador/separador';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
-  imports: [Container, Separador],
+  imports: [Container, Separador, ReactiveFormsModule],
   templateUrl: './formulario-contato.html',
-  styleUrl: './formulario-contato.css'
+  styleUrl: './formulario-contato.css',
 })
 export class FormularioContato {
+  contatoForm!: FormGroup;
 
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Matheus'),
+      telefone: new FormControl(''),
+      email: new FormControl(''),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl(''),
+    });
+  }
 }

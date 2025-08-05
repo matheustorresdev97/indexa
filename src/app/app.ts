d0036468611e4323964e90ae0aee1016e66cc768
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-05 18:36:56 -0300

    add router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 
-import agenda from './agenda.json';
-import { ContatoInterface } from './interfaces/contato';
-import { FormsModule } from '@angular/forms';
 import { FormularioContato } from './pages/formulario-contato/formulario-contato';
 import { ListaContatos } from './pages/lista-contatos/lista-contatos';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormularioContato, ListaContatos],
+  imports: [FormularioContato, ListaContatos, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

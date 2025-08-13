4e36b2e28452675687dbab04e7776aa6f0f89126
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-12 21:18:10 -0300

    feat: consumindo e iterando json da agenda e exibindo tudo com  a primeira letra maisuculas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Container } from "./components/container/container";
-import { Header } from "./components/header/header";
-import { Separator } from "./components/separator/separator";
-import { Contato } from "./components/contato/contato";
+import { Container } from './components/container/container';
+import { Header } from './components/header/header';
+import { Separator } from './components/separator/separator';
+import { Contato } from './components/contato/contato';
+
+import agenda from './agenda.json';
+import { ContatoInterface } from '../interfaces/ContatoInterface';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Container, Header, Separator, Contato],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('indexa');
 
-    alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: ContatoInterface[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string): ContatoInterface[] {
+    return this.contatos.filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }
---
+++ src/interfaces/ContatoInterface.ts
@@ -0,0 +1,5 @@
+export interface ContatoInterface {
+  id: number
+  nome: string
+  telefone: string
+}

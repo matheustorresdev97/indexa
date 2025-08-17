9891a193e6a005991a6d6d9267cee695e58de575
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-17 17:57:48 -0300

    feat: utilizando localStorage

--- src/app/services/contato.ts
+++ src/app/services/contato.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { ContatoInterface } from '../../interfaces/ContatoInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Contato {
-  
+  private contatos: ContatoInterface[] = [
+    { id: 1, nome: 'Ana', telefone: '29 278869420' },
+    { id: 2, nome: 'Ágata', telefone: '38 128451235' },
+    { id: 3, nome: 'Bruno', telefone: '95 695521583' },
+    { id: 4, nome: 'Beatriz', telefone: '25 854986459' },
+    { id: 5, nome: 'Carlos', telefone: '94 543197849' },
+    { id: 7, nome: 'Daniel', telefone: '56 613692441' },
+  ];
+
+  constructor() {
+    const contatosLocalStorageString = localStorage.getItem('contatos');
+    const contatosLocalStorage = contatosLocalStorageString
+      ? JSON.parse(contatosLocalStorageString)
+      : null;
+
+    this.contatos = contatosLocalStorage || null;
+
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
+
+  obterContatos() {
+    return this.contatos;
+  }
 }

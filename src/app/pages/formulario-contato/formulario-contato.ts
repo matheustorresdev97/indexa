41ac73a369d2d6c2a2371ae80f606b9d80b8d544
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-17 11:12:33 -0300

    feat: controlando os campos com FormControl

--- src/app/pages/formulario-contato/formulario-contato.ts
+++ src/app/pages/formulario-contato/formulario-contato.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { Container } from "../../components/container/container";
-import { Separator } from "../../components/separator/separator";
+import { Container } from '../../components/container/container';
+import { Separator } from '../../components/separator/separator';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-contato',
-  imports: [Container, Separator],
+  imports: [CommonModule, Container, Separator, ReactiveFormsModule],
   templateUrl: './formulario-contato.html',
-  styleUrl: './formulario-contato.css'
+  styleUrl: './formulario-contato.css',
 })
 export class FormularioContato {
+  contatoForm!: FormGroup;
 
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Matheus'),
+      telefone: new FormControl('99 9999-9999'),
+      email: new FormControl('matheus@email.com'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('Olá Mundo!'),
+    });
+  }
 }
